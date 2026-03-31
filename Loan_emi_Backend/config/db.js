605cba99e152ec4525f39bed19475a621867bcfe
mohammedirfan78698 <focusboost346@gmail.com>
2026-03-31 18:29:31 +0530

    db.js is updated

--- Loan_emi_Backend/config/db.js
+++ Loan_emi_Backend/config/db.js
@@ -1,11 +1,14 @@
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-
     console.log("✅ Mongo Connected");
     console.log("📂 Database Name:", conn.connection.name);
+    console.log("🌍 Host:", conn.connection.host);
   } catch (error) {
     console.error("DB Error:", error.message);
     process.exit(1);
