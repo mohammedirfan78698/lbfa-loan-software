493e4fac3c7fc6747c03b4ca8021791a89264615
mohammedirfan78698 <focusboost346@gmail.com>
2026-03-31 14:51:00 +0530

    all are updated

--- Loan_emi_Backend/server.js
+++ Loan_emi_Backend/server.js
@@ -24,7 +24,12 @@ dotenv.config();
 const app = express();
 
 // ================== MIDDLEWARE ==================
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
